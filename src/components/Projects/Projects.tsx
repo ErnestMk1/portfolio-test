d17eb48e794f98558441f0d796a4e0f6162c6ca9
ernest <a1@tcs-MacBook-Air.local>
2022-10-31 15:00:33 +0200

    added some responsobility. Fixed tab

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -25,21 +25,6 @@ const Projects = () => {
       description: 'Design & Development',
       imgUrl: projImg3,
     },
-    {
-      title: 'Business Startup',
-      description: 'Design & Development',
-      imgUrl: projImg1,
-    },
-    {
-      title: 'Business Startup',
-      description: 'Design & Development',
-      imgUrl: projImg2,
-    },
-    {
-      title: 'Business Startup',
-      description: 'Design & Development',
-      imgUrl: projImg3,
-    }
   ];
 
   return (
@@ -58,7 +43,6 @@ const Projects = () => {
 
             <Tab.Container id="projects-tabs" defaultActiveKey="first">
               <Nav
-                onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
                 className="nav-pills mb-5 justify-content-center align-items-center"
                 id="pills-tab"
               >
